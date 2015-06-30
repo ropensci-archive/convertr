19ac45fd6b480c03894416cf008d9b3940ca1b56
Gordon Shotwell <gordon.shotwell@gmail.com>
2015-06-30 18:30:01 -0300

    Added Wikipedia units

--- data_retrieval/data cleaning.R
+++ data_retrieval/data cleaning.R
@@ -1,8 +1,12 @@
 library(readr)
 library(dplyr)
-conversion_table <- read_csv("conv.csv")
+conversion_table <- read_csv("data_retrieval/conv.csv")
 names(conversion_table) <- tolower(names(conversion_table))
 names(conversion_table) <- gsub(" ", "_", names(conversion_table))
-conversion_table <- conversion_table[, 1:10]
 conversion_table <- filter(conversion_table, !is.na(a))
-save(conversion_table, file = "data/conversion table.RData")
+
+if(length(conversion_table$catalog_symbol) == length(unique(conversion_table$catalog_symbol))){
+  save(conversion_table, file = "data/conversion table.RData")
+} else {
+  stop("catalog symbols not unique")
+}
