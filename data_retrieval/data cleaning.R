0c93cfb9810d6535b46e52506bd738dd18cddd55
Gordon Shotwell <gordon.shotwell@gmail.com>
2015-07-01 10:55:27 -0300

    converted to data.frame to resolve datatables Ajax error

--- data_retrieval/data cleaning.R
+++ data_retrieval/data cleaning.R
@@ -4,6 +4,9 @@ conversion_table <- read_csv("data_retrieval/conv.csv")
 names(conversion_table) <- tolower(names(conversion_table))
 names(conversion_table) <- gsub(" ", "_", names(conversion_table))
 conversion_table <- filter(conversion_table, !is.na(a))
+conversion_table <- as.data.frame(conversion_table)
+
+
 
 if(length(conversion_table$catalog_symbol) == length(unique(conversion_table$catalog_symbol))){
   save(conversion_table, file = "data/conversion table.RData")
