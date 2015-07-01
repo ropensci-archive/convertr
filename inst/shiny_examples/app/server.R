98527bc73250777125069c32fc45bab481a4ffe2
Gordon Shotwell <gordon.shotwell@gmail.com>
2015-07-01 10:55:48 -0300

    fixed wrong base unit UI bug

--- inst/shiny_examples/app/server.R
+++ inst/shiny_examples/app/server.R
@@ -1,10 +1,12 @@
 
 shinyServer(function(input, output) {
 
+  output$text <- renderText(input$base_unit)
+
   output$conversion <- renderDataTable({
     df <- conversion_table
     if( input$base_unit != "All" ){
-      df <- df[ df$base_unit %in% input$base_unit, ]
+      df <- df[ df$base_unit == input$base_unit, ]
     }
     df
     })
