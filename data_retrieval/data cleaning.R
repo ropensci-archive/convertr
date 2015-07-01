b5326ede91198b1ea0cbb57c36a612634bb84ce7
Gordon Shotwell <gordon.shotwell@gmail.com>
2015-07-01 13:16:17 -0300

    removed non-numeric conversion factors

--- data_retrieval/data cleaning.R
+++ data_retrieval/data cleaning.R
@@ -6,6 +6,10 @@ names(conversion_table) <- gsub(" ", "_", names(conversion_table))
 conversion_table <- filter(conversion_table, !is.na(a))
 conversion_table <- as.data.frame(conversion_table)
 
+conversion_table$a <- as.numeric(conversion_table$a)
+conversion_table$b <- as.numeric(conversion_table$b)
+conversion_table$c <- as.numeric(conversion_table$c)
+conversion_table$d <- as.numeric(conversion_table$d)
 
 
 if(length(conversion_table$catalog_symbol) == length(unique(conversion_table$catalog_symbol))){
