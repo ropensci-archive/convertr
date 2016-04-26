6c393d9c981375806eae25a28ef30a6cc2f5ef08
gordon <gordon>
2016-04-26 14:12:29 -0300

    Added wikipedia units, resolved bug with degree Fahrenheit conversion.

--- data_retrieval/data cleaning.R
+++ data_retrieval/data cleaning.R
@@ -1,6 +1,9 @@
 library(readr)
 library(dplyr)
 conversion_table <- read_csv("data_retrieval/conv.csv")
+conversion_table <- conversion_table[, 1:10]
+conversion_table$name
+
 names(conversion_table) <- tolower(names(conversion_table))
 names(conversion_table) <- gsub(" ", "_", names(conversion_table))
 conversion_table <- filter(conversion_table, !is.na(a))
@@ -13,7 +16,7 @@ conversion_table$d <- as.numeric(conversion_table$d)
 
 
 if(length(conversion_table$catalog_symbol) == length(unique(conversion_table$catalog_symbol))){
-  save(conversion_table, file = "data/conversion table.RData")
+  save(conversion_table, file = "data/conversion_table.RData")
 } else {
   stop("catalog symbols not unique")
 }
